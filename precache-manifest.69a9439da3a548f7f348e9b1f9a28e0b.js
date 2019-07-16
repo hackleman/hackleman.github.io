f9229e8540ca8495964d9f8bb3c0a8461547fcf7
hackleman <hackleman.jason@gmail.com>
2019-07-16 15:28:04 -0500

    Updates

---
+++ precache-manifest.69a9439da3a548f7f348e9b1f9a28e0b.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "6b92781ad9b8b40503d28b103371a6d0",
+    "url": "/index.html"
+  },
+  {
+    "revision": "3da0f22e98fad260c00e",
+    "url": "/static/css/main.8ef63b32.chunk.css"
+  },
+  {
+    "revision": "2315f74ce3c99316133d",
+    "url": "/static/js/2.3310f33a.chunk.js"
+  },
+  {
+    "revision": "3da0f22e98fad260c00e",
+    "url": "/static/js/main.6da19034.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.054774adbe886ee6e3c29227ef1745b5.js"
+  "/precache-manifest.69a9439da3a548f7f348e9b1f9a28e0b.js"
 );
 
 self.addEventListener('message', (event) => {
