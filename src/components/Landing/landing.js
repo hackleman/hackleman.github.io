31974e28a5e7086f2fff5589afcbe2513a89db8c
hackleman <hackleman.jason@gmail.com>
2019-07-17 21:07:37 -0500

    landing page responsive

--- src/components/Landing/landing.js
+++ src/components/Landing/landing.js
@@ -9,19 +9,26 @@ function Landing() {
             <div className = "header">Hello.</div>
         </div>
         <div className = "body-container">
-            <div className = "body">My name is Jason.  I'm a full-stack developer with a passion
-            for Machine Learning and Data Visualization.  Scroll down to see some of my work.
+            <div className = "body">
+              <p>My name is Jason. </p>
+             <p>I'm a full-stack developer with a passion
+            for Machine Learning and Data Visualization.</p> 
+            <p>Scroll down to see some of my work.</p> 
             </div>
         </div>
-        <div className = "resume-button">
-          <Button id = "landing-button-1" fluid inverted >Resume.</Button>
+        <div className = "button-container">
+
+            <div className = "resume-button">
+              <Button id = "landing-button-1" fluid inverted >Resume.</Button>
+            </div>
+            <div className = "contact-button">
+              <Button id = "landing-button-1" fluid inverted >Contact.</Button>
+            </div>
+            <div className = "projects-button">
+              <Button id = "landing-button-1" fluid inverted >Projects.</Button>
+            </div>
         </div>
-        <div className = "contact-button">
-          <Button id = "landing-button-1" fluid inverted >Contact.</Button>
-        </div>
-        <div className = "projects-button">
-          <Button id = "landing-button-1" fluid inverted >Projects.</Button>
-        </div>
+
 
     </div>
   );
